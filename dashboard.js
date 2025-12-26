359c3a249db7c083116638bbaf51d13dc76a707e
kishan kumar singh <mr.kk.singh1111@gmail.com>
2025-12-26 14:55:58 +0530

    Create dashboard.js

---
+++ dashboard.js
@@ -0,0 +1,28 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth, onAuthStateChanged, signOut } 
+from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBA6HoJ3TuuZI1Mx1Z38rxvdW9J9a9xu8A",
+  authDomain: "merox-ai-mirror.firebaseapp.com",
+  projectId: "merox-ai-mirror",
+  storageBucket: "merox-ai-mirror.firebasestorage.app",
+  messagingSenderId: "69028024588",
+  appId: "1:69028024588:web:f374b0e927adfe839ff929"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// 🔐 Protect dashboard
+onAuthStateChanged(auth, (user) => {
+  if (!user) {
+    window.location.href = "./index.html";
+  }
+});
+
+window.logout = () => {
+  signOut(auth).then(() => {
+    window.location.href = "./index.html";
+  });
+};
