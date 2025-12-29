89737fe1b21b0046485a80fdaab3fc50eb52f71b
kishan kumar singh <mr.kk.singh1111@gmail.com>
2025-12-29 11:35:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
+const loginTab = document.getElementById("loginTab");
+const signupTab = document.getElementById("signupTab");
+const loginForm = document.getElementById("loginForm");
+const signupForm = document.getElementById("signupForm");
+
+loginTab.addEventListener("click", () => {
+  loginTab.classList.add("active");
+  signupTab.classList.remove("active");
+
+  loginForm.classList.remove("hidden");
+  signupForm.classList.add("hidden");
+});
+
+signupTab.addEventListener("click", () => {
+  signupTab.classList.add("active");
+  loginTab.classList.remove("active");
+
+  signupForm.classList.remove("hidden");
+  loginForm.classList.add("hidden");
+});
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import {
   getAuth,
